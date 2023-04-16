cef96d6a48f0f05112b48606759114c168832b1d
Andrew Schmelyun <schmelyun@gmail.com>
2023-04-16 03:57:10 -0400

    Fixes #16, checks for OPENAI_API_KEY before writing to .env, same with PHP configs

--- startup.sh
+++ startup.sh
@@ -13,11 +13,13 @@ if [ -z "$MEMORY_LIMIT" ]; then
     MEMORY_LIMIT="512M"
 fi
 
-echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
+# Only add this line if it's not present in the .env file
+grep -qxF "OPENAI_API_KEY=$OPENAI_API_KEY" .env || echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
 
-echo "memory_limit = $MEMORY_LIMIT" >> /usr/local/etc/php/php.ini
-echo "upload_max_filesize = $UPLOAD_MAX_FILESIZE" >> /usr/local/etc/php/php.ini
-echo "post_max_size = $UPLOAD_MAX_FILESIZE" >> /usr/local/etc/php/php.ini
+# Only add these lines if they're not present in the php.ini file
+grep -qxF "memory_limit = $MEMORY_LIMIT" /usr/local/etc/php/php.ini || echo "memory_limit = $MEMORY_LIMIT" >> /usr/local/etc/php/php.ini
+grep -qxF "upload_max_filesize = $UPLOAD_MAX_FILESIZE" /usr/local/etc/php/php.ini || echo "upload_max_filesize = $UPLOAD_MAX_FILESIZE" >> /usr/local/etc/php/php.ini
+grep -qxF "post_max_size = $UPLOAD_MAX_FILESIZE" /usr/local/etc/php/php.ini || echo "post_max_size = $UPLOAD_MAX_FILESIZE" >> /usr/local/etc/php/php.ini
 
 php artisan key:generate
 
