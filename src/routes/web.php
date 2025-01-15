e79430275688571de3d8076018e5de635e0fc2ac
Andrew Schmelyun <schmelyun@gmail.com>
2025-01-15 00:47:50 -0500

    Initial Dashboard view, eslint tweaks

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\Media;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Dashboard', [
+        'media' => Media::all(),
+    ]);
 });
