dcaafa5fabbccbc73756bf14cae64eb1f1f51316
Andrew Schmelyun <schmelyun@gmail.com>
2025-01-15 00:23:35 -0500

    Adds plugin-vue to vite config

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
