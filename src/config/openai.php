653b1fbb6ee433cefc5108d2b92b3c3eaa1da1d3
Andrew Schmelyun <schmelyun@gmail.com>
2025-05-26 00:32:05 -0400

    Allow configuring OpenAI base URI for self-hosted models

--- src/config/openai.php
+++ src/config/openai.php
@@ -14,5 +14,6 @@ return [
 
     'api_key' => env('OPENAI_API_KEY'),
     'organization' => env('OPENAI_ORGANIZATION'),
+    'base_uri' => env('OPENAI_API_BASE_URI', 'https://api.openai.com/v1'),
 
 ];
